10262316c6a1f52fe6ddccc6ecc66eb37cf87e86
Ghanshyam S <ghanshyamseervi1296@gmail.com>
2026-07-01 21:58:14 +0530

    Delete pn9.py

--- pn9.py
+++
@@ -1,9 +0,0 @@
-ls=ls=[12,23,34,"hello",(23,54,78),[12,23,54,"hello"]]
-ln=len(ls)
-for i in range(-1,-ln-1,-1):
-    print(ls[i],end=" ")
-
-print("\nanother")
-
-for i in range(ln-1,-1,-1):
-     print(ls[i],end=" ")
